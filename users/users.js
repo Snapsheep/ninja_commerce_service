501588f13a6e6577f42d181e1181ead773e91318
jayudev <jayudev@jayudevs.local>
2020-07-31 20:56:26 +0700

    temp user

---
+++ users/users.js
@@ -0,0 +1,21 @@
+const express = require('express');
+// const mongodb = require('./config/db');
+const port = process.env.port || 4003;
+
+const app = express();
+
+// const userRoutes = require('./routes/user.routes');
+
+mongodb.connectDB();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send("Hello ninjs commerce");
+});
+
+// app.use('/users', userRoutes);
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}!`);
+});
