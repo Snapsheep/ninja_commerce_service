fb52dc71ea6abeca032c7d6f50d4e1e14ea17f4e
jayudev <jayudev@jayudevs.local>
2020-07-31 22:08:56 +0700

    get try

--- orders/controller/orders.controller.js
+++ orders/controller/orders.controller.js
@@ -1,16 +1,41 @@
 const OrderModel = require("../model/orders.model");
 
 exports.createOrder = async (req, res, next) => {
-  const createModel = await OrderModel.create(req.body);
-  res.status(201).json(createModel);
+  try {
+    const createModel = await OrderModel.create(req.body);
+    res.status(201).json(createModel);
+  } catch (err) {
+    res.status(400).json({
+      error: true,
+      msg: err
+    })
+  }
 }
 
 exports.cancelOrder = async (req, res, next) => {
-  const deleteOrder = await OrderModel.findByIdAndDelete(req.params.orderId);
-  res.status(200).json(deleteOrder);
+  try {
+    const deleteOrder = await OrderModel.findByIdAndDelete(req.params.orderId);
+    res.status(200).json(deleteOrder);
+  } catch (err) {
+    res.status(400).json({
+      error: true,
+      msg: err
+    })
+  }
 }
 
 exports.getOrderItem = async (req, res, next) => {
-  const getOrderId = await OrderModel.findById(req.params.orderId);
-  res.status(200).json(getOrderId);
+  try {
+    const getOrderId = await OrderModel.findById(req.params.orderId);
+    if (getOrderId) {
+      res.status(200).json(getOrderId);
+    } else {
+      res.status(404).json({ err: true, msg: "Data not found." });
+    }
+  } catch (err) {
+    res.status(400).json({
+      error: true,
+      msg: err
+    })
+  }
 }
