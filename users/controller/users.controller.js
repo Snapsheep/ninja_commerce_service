d693af454f3edddf4baafd215952a63c05a0c2f3
jayudev <jayudev@jayudevs.local>
2020-07-31 21:32:40 +0700

    user use jwt

--- users/controller/users.controller.js
+++ users/controller/users.controller.js
@@ -28,3 +28,34 @@ exports.createUser = async (req, res, next) => {
   const createModel = await UserModel.create(data);
   res.status(201).json(createModel);
 }
+
+exports.loginUser = async (req, res, next) => {
+  const { password, email } = req.body;
+
+  if (!email || !password) {
+    return res
+      .status(400)
+      .json({ error: true, msg: 'Some values are missing.' });
+  }
+
+  if (!Helper.isValidEmail(email)) {
+    return res
+      .status(400)
+      .json({ error: true, msg: 'Please enter a valid email address.' });
+  }
+
+  const resUser = await UserModel.find({ "email": email });
+  console.log(resUser[0]);
+
+
+  if (!Helper.comparePassword(resUser[0].password, password)) {
+    return res.status(400).json({
+      error: true,
+      msg: 'The credentials you provided is incorrect.',
+    });
+  }
+
+  const token = Helper.generateToken(resUser[0]._id);
+
+  res.status(200).json({ token });
+}
--- users/routes/user.routes.js
+++ users/routes/user.routes.js
@@ -3,5 +3,6 @@ const userController = require("../controller/users.controller");
 const router = express.Router();
 
 router.post('/create-user', userController.createUser);
+router.post('/login', userController.loginUser);
 
 module.exports = router;
