4c5f9b9d5e6c3de70b7170d30ea39982869d286a
jayudev <jayudev@jayudevs.local>
2020-07-31 20:19:43 +0700

    update get product by id

--- products/controller/products.controller.js
+++ products/controller/products.controller.js
@@ -4,3 +4,13 @@ exports.createProduct = async (req, res, next) => {
   const createModel = await ProductModel.create(req.body);
   res.status(201).json(createModel);
 }
+
+exports.getProducts = async (req, res, next) => {
+  const allProducts = await ProductModel.find({});
+  res.status(200).json(allProducts);
+}
+
+exports.getProductItem = async (req, res, next) => {
+  const getProductId = await ProductModel.findById(req.params.productId);
+  res.status(200).json(getProductId);
+}
--- products/routes/product.routes.js
+++ products/routes/product.routes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const productController = require("../controller/products.controller");
 const router = express.Router();
 
-router.post("/", productController.createProduct);
+router.post('/', productController.createProduct);
+router.get('/', productController.getProducts);
+router.get('/:productId', productController.getProductItem);
 
 module.exports = router;
